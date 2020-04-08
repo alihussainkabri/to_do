d5361a702887be3de1c18470ae900c999dce5af9
alihussainkabri <alihussainkabri52@gmail.com>
2020-04-08 23:05:36 +0530

    modified first

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from to_do_app import views
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
 urlpatterns = [
     path('',views.index,name='index'),
     path('login/',views.signin,name='signin'),
@@ -11,4 +14,6 @@ urlpatterns = [
     path('ajax/status/reactivate',views.ajax_status_reactivate,name='ajax_status_reactivate'),
     path('ajax/status/delete',views.ajax_status_delete,name='ajax_status_delete'),
     path('ajax/task_details/',views.ajax_task_details,name='ajax_task_details'),
+    url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
 ]
